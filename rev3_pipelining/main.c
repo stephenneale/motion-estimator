1ec235629df570a0469a71d8e098f08930ec55ce
Stephen Neale <stephenneale@uvic.ca>
2024-07-24 15:47:02 -0700

    Signage for pipelining

--- rev3_pipelining/main.c
+++ rev3_pipelining/main.c
@@ -5,52 +5,51 @@
 #define BLOCK_SIZE 16
 #define SEARCH_RANGE 7
 
-// Function to calculate SAD for a block with software pipelining
+// Function to calculate SAD for a block using software pipelining
 unsigned short calculate_sad(unsigned char *current, unsigned char *reference, int x, int y, int ref_x, int ref_y, int width, int height) {
     unsigned short sad = 0;
+    int i = 0, j = 0;
 
-    // Prologue: Handle the first few iterations to start filling the pipeline
-    for (int i = 0; i < BLOCK_SIZE; i++) {
-        for (int j = 0; j < 2; j++) {
+    // Prologue
+    for (; i < 3; i++) {
+        for (j = 0; j < BLOCK_SIZE; j++) {
             int cur_index = (y + i) * width + (x + j);
             int ref_index = (ref_y + i) * width + (ref_x + j);
             if (cur_index < width * height && ref_index < width * height) {
-                sad += abs(current[cur_index] - reference[ref_index]);
+                sad += abs((int)current[cur_index] - (int)reference[ref_index]);
             }
         }
     }
 
-    // Steady-state loop: Main loop with pipelining
-    for (int i = 0; i < BLOCK_SIZE; i++) {
-        for (int j = 2; j < BLOCK_SIZE - 2; j++) {
-            int cur_index0 = (y + i) * width + (x + j - 2);
-            int ref_index0 = (ref_y + i) * width + (ref_x + j - 2);
-            int cur_index1 = (y + i) * width + (x + j - 1);
-            int ref_index1 = (ref_y + i) * width + (ref_x + j - 1);
-            int cur_index2 = (y + i) * width + (x + j);
-            int ref_index2 = (ref_y + i) * width + (ref_x + j);
-            int cur_index3 = (y + i) * width + (x + j + 1);
-            int ref_index3 = (ref_y + i) * width + (ref_x + j + 1);
-            int cur_index4 = (y + i) * width + (x + j + 2);
-            int ref_index4 = (ref_y + i) * width + (ref_x + j + 2);
+    // Steady state loop
+    for (; i < BLOCK_SIZE - 3; i++) {
+        int cur_indices[4], ref_indices[4];
+        unsigned short sads[4] = {0};
 
-            if (cur_index4 < width * height && ref_index4 < width * height) {
-                sad += abs(current[cur_index0] - reference[ref_index0]);
-                sad += abs(current[cur_index1] - reference[ref_index1]);
-                sad += abs(current[cur_index2] - reference[ref_index2]);
-                sad += abs(current[cur_index3] - reference[ref_index3]);
-                sad += abs(current[cur_index4] - reference[ref_index4]);
+        // Load indices
+        for (int k = 0; k < 4; k++) {
+            for (j = 0; j < BLOCK_SIZE; j++) {
+                cur_indices[k] = (y + i + k) * width + (x + j);
+                ref_indices[k] = (ref_y + i + k) * width + (ref_x + j);
+                if (cur_indices[k] < width * height && ref_indices[k] < width * height) {
+                    sads[k] += abs((int)current[cur_indices[k]] - (int)reference[ref_indices[k]]);
+                }
             }
+        }
+
+        // Sum the intermediate sads
+        for (int k = 0; k < 4; k++) {
+            sad += sads[k];
         }
     }
 
-    // Epilogue: Handle the last few iterations to drain the pipeline
-    for (int i = 0; i < BLOCK_SIZE; i++) {
-        for (int j = BLOCK_SIZE - 2; j < BLOCK_SIZE; j++) {
+    // Epilogue
+    for (; i < BLOCK_SIZE; i++) {
+        for (j = 0; j < BLOCK_SIZE; j++) {
             int cur_index = (y + i) * width + (x + j);
             int ref_index = (ref_y + i) * width + (ref_x + j);
             if (cur_index < width * height && ref_index < width * height) {
-                sad += abs(current[cur_index] - reference[ref_index]);
+                sad += abs((int)current[cur_index] - (int)reference[ref_index]);
             }
         }
     }
@@ -59,18 +58,18 @@ unsigned short calculate_sad(unsigned char *current, unsigned char *reference, i
 }
 
 // Function to find the best match for a block in the reference frame
-void find_best_match(unsigned char *current, unsigned char *reference, int x, int y, int width, int height, char *best_x, char *best_y) {
+void find_best_match(unsigned char *current, unsigned char *reference, int x, int y, int width, int height, signed char *best_x, signed char *best_y) {
     unsigned short min_sad = USHRT_MAX;
     for (int i = -SEARCH_RANGE; i <= SEARCH_RANGE; i++) {
         for (int j = -SEARCH_RANGE; j <= SEARCH_RANGE; j++) {
             int ref_x = x + j;
             int ref_y = y + i;
-            if (ref_x >= 0 && ref_y >= 0 && ref_x + BLOCK_SIZE < width && ref_y + BLOCK_SIZE < height) {
+            if (ref_x >= 0 && ref_y >= 0 && ref_x + BLOCK_SIZE <= width && ref_y + BLOCK_SIZE <= height) {
                 unsigned short sad = calculate_sad(current, reference, x, y, ref_x, ref_y, width, height);
                 if (sad < min_sad) {
                     min_sad = sad;
-                    *best_x = (char)(ref_x - x);
-                    *best_y = (char)(ref_y - y);
+                    *best_x = (signed char)(ref_x - x);
+                    *best_y = (signed char)(ref_y - y);
                 }
             }
         }
@@ -78,10 +77,10 @@ void find_best_match(unsigned char *current, unsigned char *reference, int x, in
 }
 
 // Function to perform motion estimation for the entire frame
-void motion_estimation(unsigned char *current, unsigned char *reference, int width, int height, char *motion_vectors) {
+void motion_estimation(unsigned char *current, unsigned char *reference, int width, int height, signed char *motion_vectors) {
     for (int y = 0; y < height; y += BLOCK_SIZE) {
         for (int x = 0; x < width; x += BLOCK_SIZE) {
-            char best_x = 0, best_y = 0;
+            signed char best_x = 0, best_y = 0;
             find_best_match(current, reference, x, y, width, height, &best_x, &best_y);
             int index = (y / BLOCK_SIZE) * (width / BLOCK_SIZE) + (x / BLOCK_SIZE);
             motion_vectors[2 * index] = best_x;
@@ -119,7 +118,7 @@ int main() {
     unsigned char *current_frame = read_image_from_text("../utils/current_frame.txt", width, height);
     unsigned char *reference_frame = read_image_from_text("../utils/reference_frame.txt", width, height);
 
-    char *motion_vectors = (char *)malloc(2 * (width / BLOCK_SIZE) * (height / BLOCK_SIZE) * sizeof(char));
+    signed char *motion_vectors = (signed char *)malloc(2 * (width / BLOCK_SIZE) * (height / BLOCK_SIZE) * sizeof(signed char));
     motion_estimation(current_frame, reference_frame, width, height, motion_vectors);
 
     // Print motion vectors
